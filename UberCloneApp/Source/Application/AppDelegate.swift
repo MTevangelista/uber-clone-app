095460f160bd0629364d13c031cf297e85acf4b7
Matheus Evangelista <matheus.evangelista@g.globo>
2022-01-27 20:29:51 -0300

    Initial setup

--- UberCloneApp/Source/Application/AppDelegate.swift
+++ UberCloneApp/Source/Application/AppDelegate.swift
@@ -1,16 +1,7 @@
-//
-//  AppDelegate.swift
-//  UberCloneApp
-//
-//  Created by matheus.evangelista on 23/01/22.
-//
-
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
---
+++ UberCloneApp/Source/Module/Login/ViewController/LoginController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class LoginController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+    
+}
